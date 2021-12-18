3b1fad84f1f8aded4751d8992d19add26436dc96
greeg01 <greeg01-dev>
2021-12-18 14:35:58 +0900

    Build Application

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use bevy::{prelude::{App, DefaultPlugins, ClearColor, Color}, window::WindowDescriptor};
+
 fn main() {
-    println!("Hello, world!");
+   App::build()
+        .insert_resource(WindowDescriptor {
+            width: 800.0,
+            height: 500.0,
+            title: String::from("Catching Apple Game"),
+            resizable: false,
+            ..Default::default()
+        })
+        .insert_resource(ClearColor(Color::WHITE))
+        .add_plugins(DefaultPlugins)
+        .run();
 }
